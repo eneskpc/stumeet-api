0b1cd237b39dee0d86755bd98b6ecdc708112297
Enes Kapucu <enes.kapucu@outlook.com.tr>
2019-03-06 23:57:49 +0300

    init solution

---
+++ Stumeet.Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Stumeet.Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ Stumeet.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Stumeet.Core
+{
+    public class Class1
+    {
+    }
+}
