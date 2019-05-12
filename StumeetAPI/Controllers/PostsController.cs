5005382532ab32eeba12424b7c32cb7bc087ec21
Enes Kapucu <enes.kapucu@outlook.com.tr>
2019-05-13 01:09:48 +0300

    postscontrollerın yazımına başlandı(tuğba)

--- StumeetAPI/Controllers/PostsController.cs
+++ StumeetAPI/Controllers/PostsController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using StumeetAPI.Business.Abstract;
+using StumeetAPI.DTOs;
+using StumeetAPI.Entities.Concrete;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,25 +14,76 @@ namespace StumeetAPI.Controllers
     [Route("[controller]")]
     public class PostsController : Controller
     {
+        private IPostService _postManager;
+        private IAuthenticationService _authManager;
+
+        public PostsController(IPostService postManager, IAuthenticationService authManager)
+        {
+            _postManager = postManager;
+            _authManager = authManager;
+        }
+
         // GET: api/<controller>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<ActionResult> GetPosts()
         {
-            return new string[] { "value1", "value2" };
+            var errorDetail = await _authManager.CheckUser(Request.Headers["Authorization"]);
+            if (errorDetail.StatusCode != 200)
+            {
+                return Unauthorized(errorDetail);
+            }
+            User currentUser = errorDetail.Data;
+            var postList = await _postManager.GetAll(u => u.IsDeleted != true);
+            if (postList == null)
+            {
+                return NotFound();
+            }
+            return Ok(postList);
         }
 
-        // GET api/<controller>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<controller>
+        // POST: api/<controller>
         [HttpPost]
-        public void Post([FromBody]string value)
+        public async Task<ActionResult> AddPost([FromBody] PostForAdd postForAdd)
         {
+            var errorDetail = await _authManager.CheckUser(Request.Headers["Authorization"]);
+            if (errorDetail.StatusCode != 200)
+            {
+                return Unauthorized(errorDetail);
+            }
+            User currentUser = errorDetail.Data;
+            var recordedPost = await _postManager.Add(new Post
+            {
+                UserId = postForAdd.UserId,
+                PostContent = postForAdd.PostContent,
+                CreationDate = DateTime.Now,
+                IsDeleted = false
+            });
+            if (recordedPost == null)
+            {
+                return NotFound();
+            }
+            return Ok(recordedPost);
         }
+
+        // POST: api/<controller>
+        [HttpPost]
+        public async Task<ActionResult> UpdatePost([FromBody] Post postForUpdate)
+        {
+            var errorDetail = await _authManager.CheckUser(Request.Headers["Authorization"]);
+            if (errorDetail.StatusCode != 200)
+            {
+                return Unauthorized(errorDetail);
+            }
+            User currentUser = errorDetail.Data;
+            postForUpdate.UpdatedDate = DateTime.Now;
+            var recordedPost = await _postManager.Add(postForUpdate);
+            if (recordedPost == null)
+            {
+                return NotFound();
+            }
+            return Ok(recordedPost);
+        }
+
 
         // PUT api/<controller>/5
         [HttpPut("{id}")]
---
+++ StumeetAPI/DTOs/PostForAdd.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StumeetAPI.DTOs
+{
+    public class PostForAdd
+    {
+        public int UserId { get; set; }
+        public string PostContent { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
